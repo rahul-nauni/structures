9fdb9841bb3671b25780d1c3cb3ed82d21346c7b
Rahul Nauni <rahulnauni@outlook.com>
2024-11-17 18:56:41 +0000

    Export Items slice in deque

--- deque/deque.go
+++ deque/deque.go
@@ -7,23 +7,23 @@ import "errors"
 var ErrEmptyDeque = errors.New("queue is empty")
 
 type Deque[T any] struct {
-	items []T
+	Items []T
 }
 
 func New[T any]() *Deque[T] {
 	d := &Deque[T]{
-		items: make([]T, 0), // Empty slice of type T
+		Items: make([]T, 0), // Empty slice of type T
 	}
 
 	return d
 }
 
 func (d *Deque[T]) AppendLeft(value T) {
-	d.items = append([]T{value}, d.items...)
+	d.Items = append([]T{value}, d.Items...)
 }
 
 func (d *Deque[T]) Pop() T {
-	removed := d.items[len(d.items)-1]
-	d.items = d.items[:len(d.items)-1]
+	removed := d.Items[len(d.Items)-1]
+	d.Items = d.Items[:len(d.Items)-1]
 	return removed
 }
